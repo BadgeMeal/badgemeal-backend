37c2ea04739da27ffb1f3a689635b8fa14435e09
sapcy <sychoi1644@gmail.com>
2022-02-19 03:41:00 +0900

    :sparkles: 메뉴 리스트 가져오기 기능 추가

---
+++ apis/src/main/java/hack/badgemeal/apis/domain/menu/service/MenuService.java
@@ -0,0 +1,19 @@
+package hack.badgemeal.apis.domain.menu.service;
+
+import hack.badgemeal.apis.domain.menu.model.Menu;
+import hack.badgemeal.apis.domain.menu.repository.MenuRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MenuService {
+    private final MenuRepository menuRepository;
+
+    public List<Menu> getMenuList() {
+        return menuRepository.findAll();
+    }
+
+}

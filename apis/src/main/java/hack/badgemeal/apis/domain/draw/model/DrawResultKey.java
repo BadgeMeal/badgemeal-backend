fbba4c218f08409d4dd4dac8c28d1068a03b98c1
sapcy <sychoi1644@gmail.com>
2022-02-19 17:34:39 +0900

    :sparkles: 뽑기 결과 인증 여부 조회 기능 수정

---
+++ apis/src/main/java/hack/badgemeal/apis/domain/draw/model/DrawResultKey.java
@@ -0,0 +1,8 @@
+package hack.badgemeal.apis.domain.draw.model;
+
+import java.io.Serializable;
+
+public class DrawResultKey implements Serializable {
+    private String address;
+    private long round;
+}

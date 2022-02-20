cdc6061dd9ba2ab680302b16b65cbe157c0dc072
hyungi <hkgo6040@naver.com>
2022-02-20 17:37:36 +0900

    🚧 vote Contract scheduler 구현

--- apis/src/main/java/hack/badgemeal/apis/domain/ipfs/controller/IpfsController.java
+++ apis/src/main/java/hack/badgemeal/apis/domain/ipfs/controller/IpfsController.java
@@ -1,21 +1,41 @@
 package hack.badgemeal.apis.domain.ipfs.controller;
 
+import hack.badgemeal.apis.common.dto.ResultDto;
 import hack.badgemeal.apis.domain.ipfs.service.IpfsService;
-import hack.badgemeal.apis.domain.ocr.service.OcrService;
+import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
+@Api(tags = {"IPFS에 MasterNFT 이미지를 업로드하고 DB에 저장해 관리하는 Controller"})
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin("*")
+@RequestMapping("ipfs")
 public class IpfsController {
     private final IpfsService ipfsService;
 
-    @PostMapping("/api/ipfs/upload")
-    public String postIpfsImgUpload(@RequestParam("img") MultipartFile image) throws IOException {
-        return ipfsService.uploadToIPFS(image);
+    @PostMapping("/upload")
+    public Object postIpfsImgUpload(@RequestParam("menu_no") Long menu_no, @RequestParam("img") MultipartFile image){
+        ResultDto resultDto = new ResultDto();
+
+        String status = "fail";
+        try{
+            resultDto = ipfsService.uploadToIPFS(menu_no, image);
+        }catch (Exception e){
+            e.printStackTrace();
+            status =  "fail";
+            resultDto.setMsg(e.getMessage());
+        }
+
+        resultDto.setStatus(status);
+        return resultDto;
+    }
+
+    @GetMapping("/getMasterNftImage")
+    public Object postIpfsImgUpload(@RequestParam("menu_no") Long menu_no){
+         return ipfsService.getIpfsUrlByMenuNo(menu_no);
     }
 }
---
+++ apis/src/main/java/hack/badgemeal/apis/domain/ipfs/repository/IpfsRepository.java
@@ -0,0 +1,14 @@
+package hack.badgemeal.apis.domain.ipfs.repository;
+
+import hack.badgemeal.apis.domain.ipfs.model.MasterNFT;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface IpfsRepository extends JpaRepository<MasterNFT, Long> {
+    List<MasterNFT> findByMenuNoAndMintYn(Long menu_no, Long mint_yn);
+    Optional<MasterNFT> findByImageUrl(String image_url);
+}
